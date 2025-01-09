6b621609631738c742250fe8a856b8da21e5ef60
Amari Augustin <amariaugustin4@gmail.com>
2025-01-09 09:16:28 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AboutMe from './components/aboutMe.jsx'
+import AboutMe from './components/AboutMe.jsx'
 import Project from './components/Project.jsx'
 import Skills from './components/Skills.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
